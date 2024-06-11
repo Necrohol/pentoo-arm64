a28cb2be4f9bbd84c60fcec181711abbd3c79719
Michael L <mike@michaellawrenceit.com>
2024-06-10 23:12:25 -0500

    Update setup_pentoo.sh

--- scripts/setup_pentoo.sh
+++ scripts/setup_pentoo.sh
@@ -8,11 +8,17 @@ eselect repository enable pentoo
 echo "Adding keywording for ~arm64 for Pentoo repository..."
 mkdir -p /etc/portage/package.accept_keywords
 echo "*/*::pentoo ~arm64" >> /etc/portage/package.accept_keywords/pentoo_repo
-## pentoo-arm64 https://github.com/Necrohol/pentoo-arm64 "underlay" 
-echo "*/*::pentoo-arm64 ~arm64" >> /etc/portage/package.accept_keywords/pentoo-arm64_repo
 
 # Add keywording for ~arm64 for Gentoo repository
 echo "Adding keywording for ~arm64 for Gentoo repository..."
 echo "*/*::gentoo ~arm64" >> /etc/portage/package.accept_keywords/gentoo_repo
+
+# Enable the pentoo-arm64 repository
+echo "Enabling the pentoo-arm64 repository..."
+eselect repository add pentoo-arm64 git https://github.com/Necrohol/pentoo-arm64.git
+
+# Add keywording for ~arm64 for pentoo-arm64 repository
+echo "Adding keywording for ~arm64 for unofficial  pentoo-arm64 repository..."
+echo "*/*::pentoo-arm64 ~arm64" >> /etc/portage/package.accept_keywords/pentoo-arm64_repo
 
 echo "Setup complete."
